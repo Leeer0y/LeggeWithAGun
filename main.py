74698abe4512977fd0c86e3be9b69b6d65c7eda6
Leeer0y <leo@codekidz.com>
2024-06-05 13:53:45 +1000

    Physics working

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from player import Player
 #Init pygame
 pygame.init()
 
-
 screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
 
 p = Player()
 p.position = pygame.Vector2(400, 0)
@@ -14,32 +14,26 @@ p.position = pygame.Vector2(400, 0)
 ground = pygame.Rect(0, 700, 800, 10)
 
 colision_list = [ground]
-last_frame_ticks = pygame.time.get_ticks()
 
 # Gameloop
 running = True
 while running:
+    #limit FPS
+    clock.tick(60)
 
     # Delta time calculations
-    t = pygame.time.get_ticks()
-    delta_time = (t - last_frame_ticks) / 1000.0
-    last_frame_ticks = t
+    delta_time = clock.get_time() / 1000 
     
     #Check for events
     for event in pygame.event.get():
         if event.type == pygame.quit:
             running = False
 
-        p.player_controls(event, delta_time)
    
     #Set background
     screen.fill((255, 0, 0))
 
-    p.render(screen, delta_time)
+    p.render(screen, delta_time, colision_list)
     pygame.draw.rect(screen, (155, 155, 155), ground)
-
-    # Player Gravity
-    #if p.hitbox.collidelist(colision_list) >= 0 :
-     #   p.isGrounded = True
 
     pygame.display.flip()
--- player.py
+++ player.py
@@ -12,28 +12,66 @@ class Player:
         self.IsGrounded = False
 
         # Movement
-        self.walk_speed = 0
-        self.max_walk_speed = 10
-        self.walk_acceleration = 0.2
+        self.walk_speed = 0.0
+        self.max_walk_speed = 100.0
+        self.walk_acceleration = 26
+        
+        self.jumpVelocity = 90
 
         #Physics
         self.velocity = pygame.Vector2(0.0, 0.0)
-        self.gravity = 0.4
+        self.gravity = 9.8
+        self.friction = 3
 
-    def player_controls(self, event, delta_time) :
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d :
-                self.velocity.x = 20
-
-    def render(self, screen, delta_time) :
+    def player_controls(self) :
+        keys = pygame.key.get_pressed()
+        
+        # Movement
+        if keys[pygame.K_d] :
+            if self.velocity.x < self.max_walk_speed :
+                self.velocity.x += self.walk_acceleration
+        if keys[pygame.K_a] :
+            if self.velocity.x > (-self.max_walk_speed) :
+                self.velocity.x -= self.walk_acceleration
+        if keys[pygame.K_w] :
+            self.velocity.y -= self.jumpVelocity
+            #print(self.velocity * delta_time)
+                
+    def render(self, screen, delta_time, collision_list) :
         pygame.draw.circle(screen, (255, 255, 255), self.position, 15)
         
+        # Collisions
+        collisions = self.hitbox.collidelistall(collision_list)
+        if collisions :
+            #test ground
+            for i in collisions :
+                if collision_list[i].top <= self.hitbox.bottom :
+                    self.IsGrounded = True
+        else :
+            self.IsGrounded = False
+        
+
+        # Gravity
+        if self.IsGrounded == False :
+            self.velocity.y += self.gravity
+        else :
+            if self.velocity.y > 0.0 :
+                self.velocity.y = 0.0
+
+            #friction
+            if self.velocity :
+                direction_travelling = self.velocity.normalize() # The higher the velocity in a direction, the closer it will be to 1
+                if abs(self.velocity.magnitude()) > self.friction :
+                    print(direction_travelling * self.friction)
+                    self.velocity -= (direction_travelling * self.friction)
+                else :
+                    self.velocity = direction_travelling * 0
+
+        # Movement
+        self.player_controls()
+        self.position.x += self.velocity.x * delta_time
+        self.position.y += self.velocity.y * delta_time
+
         # Update hitbox
         self.hitbox.x = int(self.position.x)
         self.hitbox.y = int(self.position.y)
-
-        # Movement
-        
-
-        self.position.x += self.velocity.x * delta_time
-        self.position.y += self.velocity.y * delta_time
