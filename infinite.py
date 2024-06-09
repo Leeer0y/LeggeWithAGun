23fa2b30a01633102c24c3192a41b521de6e07ff
Leeer0y <leo@codekidz.com>
2024-06-09 15:06:15 +1000

    simple side scroll

--- infinite.py
+++ infinite.py
@@ -6,8 +6,10 @@ import gemoetry
 class Infinite(scene.Scene) :
     def __init__(self, screen: pygame.Surface, gamestatemgr) -> None:
         super().__init__(screen, gamestatemgr)
-        
+
         self.player = player.Player()
+        self.player.rect.x = 400
+
         self.ground = gemoetry.rect((0, 0, 0), pygame.Rect(0, 790, 800, 20))
 
         self.entities = pygame.sprite.Group()
@@ -16,7 +18,17 @@ class Infinite(scene.Scene) :
         self.geometry = pygame.sprite.Group()
         self.geometry.add(self.ground)
 
+        self.sidescroll_group = pygame.sprite.Group()
+        self.sidescroll_group.add(self.player)
+
+        self.sidescroll_speed = 35
+
+        self.clock = pygame.time.Clock()
+
     def update(self):
+        # Timing
+        delta_time = self.clock.get_time() / 1000
+
         # Background
         self.screen.fill((25, 25, 25))
 
@@ -26,9 +38,15 @@ class Infinite(scene.Scene) :
 
         self.geometry.draw(self.screen)
 
+        # Side sidescroll_group
+        for i in self.sidescroll_group.sprites() :
+            i.rect.x -= self.sidescroll_speed * delta_time
+
         # Colision
         entity_colisions = pygame.sprite.groupcollide(self.entities, self.geometry, False, False)
         if entity_colisions :
             for i in entity_colisions :
                 i.set_grounded(True)
+
+        self.clock.tick()
 
